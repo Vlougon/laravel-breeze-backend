a4bc60a197c8b08b6804b01880c7de468f760550
Victor <vlougongs@gmail.com>
2024-02-02 20:22:26 +0000

    Allowed Origins Updated 2!

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout', 'forgot-password', 'reset-password', 'verify-email'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout', 'forgot-password', 'reset-password', 'verify-email', 'email/verification-notification'],
 
     'allowed_methods' => ['*'],
 
