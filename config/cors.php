62edd9b9cb4447e47ff8228a702f13d40e2018af
Victor <vlougongs@gmail.com>
2024-02-02 19:59:36 +0000

    Added Paths to Cors!

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout', 'forgot-password', 'reset-password', 'verify-email'],
 
     'allowed_methods' => ['*'],
 
